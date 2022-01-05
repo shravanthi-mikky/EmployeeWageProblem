75512704703087eb23446a6ec8e45b3d395281bb
shravanthi-mikky <shravanthi27041996@gmail.com>
2022-01-05 20:27:16 +0530

    uc1 Commit

--- emp.sh
+++ emp.sh
@@ -1,2 +1,10 @@
 #! /bin/bash -x
 echo "Welcome to Employee Wage Computation Program on Master Branch"
+isPresent=1
+randomnum=$(($RANDOM%2))
+if [  isPresent -eq $randomnum ]
+then
+	echo "Employee is Present "
+else
+	echo "Employee is Absent "
+fi
