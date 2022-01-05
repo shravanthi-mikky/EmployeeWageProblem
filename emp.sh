cba5a99b0970e61ee712288480b113a50243e299
shravanthi-mikky <shravanthi27041996@gmail.com>
2022-01-05 22:58:58 +0530

    Salary For Month

--- emp.sh
+++ emp.sh
@@ -8,3 +8,30 @@ then
 else
 	echo "Employee is Absent "
 fi
+if [ $randomnum -eq 1 ]
+then
+	days=20
+        hours=8
+        wage=$(( $randomnum * $days * $hours ))
+        echo "Daily wage of employee is : " $wage
+else
+	wage=0
+	echo "Wage is " $wage
+fi
+# Adding partime employee wage
+isPartTime=1
+isFullTime=2
+empRatePerHour=20
+randomcheck=$(($RANDOM%3))
+case $randomcheck in
+	$isFullTime )
+		empHrs=8;;
+	$randomcheck )
+		empHrs=4;;
+	* )
+		empHrs=0;;
+esac
+Salary=$(($empHrs*$empRatePerHour))
+workingDays=20
+salaryForMonth=$(( $Salary * $workingDays ))
+echo $salaryForMonth
