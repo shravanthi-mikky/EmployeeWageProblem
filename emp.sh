258c7f42defe3c09efb36dc50dfee9618a3e4d5a
shravanthi-mikky <shravanthi27041996@gmail.com>
2022-01-05 21:27:14 +0530

    Adding Part time Employee wage

--- emp.sh
+++ emp.sh
@@ -2,14 +2,34 @@
 echo "Welcome to Employee Wage Computation Program on Master Branch"
 isPresent=1
 randomnum=$(($RANDOM%2))
-if [  isPresent -eq $randomnum ]
+if [ $isPresent -eq $randomnum ]
 then
 	echo "Employee is Present "
 else
-	echo "Employee is Absent "
+        echo "Employee is absent "
 fi
-#Calculate Daily Employee Wage
-days=20
-hours=8
-wage=$(( $randomnum * $days * $hours ))
-echo "Daily wage of employee is : " $wage
+if [ $randomnum -eq 1 ]
+then
+	days=20
+        hours=8
+        wage=$(( $randomnum * $days * $hours ))
+        echo "Daily wage of employee is : " $wage
+else
+	wage=0
+	echo "Wage is " $wage
+fi
+# Adding partime employee wage
+isPartTime=1
+isFullTime=2
+empRatePerHour=20
+randomcheck=$(($RANDOM%3))
+if [ $isFullTime -eq $randomcheck ]
+then
+	empHrs=8
+elif [ $isPartTime -eq $randomcheck ]
+then
+	empHrs=4
+else
+	empHrs=0
+fi
+Salary=$(($empHrs*$empRatePerHour))
