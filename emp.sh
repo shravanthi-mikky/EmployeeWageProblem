03b367b200d98125cfb15c527f3bfacc12b9bd67
shravanthi-mikky <shravanthi27041996@gmail.com>
2022-01-08 16:28:58 +0530

    Calculate wages

--- emp.sh
+++ emp.sh
@@ -1,37 +1,30 @@
-#! /bin/bash -x
 echo "Welcome to Employee Wage Computation Program on Master Branch"
-isPresent=1
-randomnum=$(($RANDOM%2))
-if [  isPresent -eq $randomnum ]
-then
-	echo "Employee is Present "
-else
-	echo "Employee is Absent "
-fi
-if [ $randomnum -eq 1 ]
-then
-	days=20
-        hours=8
-        wage=$(( $randomnum * $days * $hours ))
-        echo "Daily wage of employee is : " $wage
-else
-	wage=0
-	echo "Wage is " $wage
-fi
-# Adding partime employee wage
-isPartTime=1
-isFullTime=2
-empRatePerHour=20
-randomcheck=$(($RANDOM%3))
-case $randomcheck in
-	$isFullTime )
-		empHrs=8;;
-	$randomcheck )
-		empHrs=4;;
-	* )
-		empHrs=0;;
-esac
-Salary=$(($empHrs*$empRatePerHour))
-workingDays=20
-salaryForMonth=$(( $Salary * $workingDays ))
-echo $salaryForMonth
+#! /bin/bash -x
+partTime=1
+fullTime=2
+maxHoursInMonth=4
+empRatePerHr=20
+numOfWorkingDays=20
+totalEmployees=0
+toatlWorkingDays=0
+function getWorkingHours(){
+	case $1 in 
+		$fullTime)
+			workingHours=8
+			;;
+		$partTime)
+			workingHours=4
+			;;
+		*)
+			workingHours=0
+			;;
+	esac
+	echo $workingHours
+}
+while [[ $toatlWorkingHours -lt maxHoursInMonth && $toatlWorkingDays -lt numOfWorkingDays ]]
+do
+	((totalWorkingDays++))
+	workingHours=`${ getWorkingHours $((RANDOM%3)) }`
+	toatlWorkingHours=$(($toatlWorkingHours*$workingHours))	
+done
+totalSalary=$(($totalWorkingHours*$empRatePerHr))
